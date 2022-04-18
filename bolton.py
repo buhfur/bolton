49bd5ba5b0a097ec130ae26eaf6d0b66b9d186ae
ryanmcvicker12 <ryanmcvicker12@gmail.com>
2022-04-18 16:03:38 -0400

    changes

--- bolton.py
+++ bolton.py
@@ -1,23 +1,22 @@
+import sys 
 from selenium import webdriver 
-from selenium.webdriver.common.keys import Keys  
 
 # first goal : sign into indeed.com 
 
 
-driver  = webdriver.Firefox() 
+#args = str[] 
 
-driver.get('https://indeed.com') 
-# make sure that the sign in button has the class name "gnav-header-o4sfaq emf9s7v0"  
-sign_in_link = driver.find_element_by_link_text("Sign in") 
-
-
-
-# find sign_in button 
-
-# goto href in the button to get to the sign in page 
-
-#redirect for the browser 
+#README: Function that initially uses the first two arguments to setup credentials for the bot 
+def setup_creds():  # I don't know how stupid this is to do 
+    global EMAIL 
+    global PASS  
+    EMAIL = sys.argv[1]
+    PASS  = sys.argv[2] 
+    return (EMAIL, PASS) 
 
 
-assert "no results found " not in driver.page_source 
-driver.close()
+with driver as webdriver.Firefox() : 
+
+    setup_creds()  # establish credentials 
+
+    driver.get("https://secure.indeed.com/account/login?hl=en_US&co=US&continue=https%3A%2F%2Fwww.indeed.com%2F&tmpl=desktop&service=my&from=gnav-util-homepage&jsContinue=https%3A%2F%2Fwww.indeed.com%2F&empContinue=https%3A%2F%2Faccount.indeed.com%2Fmyaccess") 
--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,13 @@
 import unittest 
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys 
+
+class IndeedOrgSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox() 
+
+    #attempt to sign into indeed 
+    def test_indeed_search(self):
+        driver = self.driver 
+
+
