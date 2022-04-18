e3715c233e13b1b7dadb51c75f0b8ee8fe02d64d
ryanmcvicker12 <ryanmcvicker12@gmail.com>
2022-04-18 16:14:17 -0400

    changes

--- bolton.py
+++ bolton.py
@@ -15,8 +15,18 @@ def setup_creds():  # I don't know how stupid this is to do
     return (EMAIL, PASS) 
 
 
-with driver as webdriver.Firefox() : 
+with webdriver.Firefox() as driver : 
 
-    setup_creds()  # establish credentials 
+    #TODO : throw exception IndexError when no input for the email address is supplied 
+    try:
+        setup_creds() 
+
+    except IndexError:
+        print("NO email or password provided") 
+        if len(sys.argv > 3): 
+            print("Too many arguments supplied " ) 
+
+        
+    print(f"username: {0}\npassword: {1} ",setup_creds() )  # establish credentials 
 
     driver.get("https://secure.indeed.com/account/login?hl=en_US&co=US&continue=https%3A%2F%2Fwww.indeed.com%2F&tmpl=desktop&service=my&from=gnav-util-homepage&jsContinue=https%3A%2F%2Fwww.indeed.com%2F&empContinue=https%3A%2F%2Faccount.indeed.com%2Fmyaccess") 
